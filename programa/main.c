e42bb57d55517e23a219826f45d4d4b9a6fab103
fchacon20 <felipe.chacon.ossa@gmail.com>
2016-11-22 21:19:50 -0300

    Entrada lista

--- programa/main.c
+++ programa/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <wait.h>
+#include "input_handler.h"
 
 int execProgram(char* name, int size){
 	/*
@@ -52,6 +53,41 @@ int checkMem(char* name){
 	*/
 }
 
+void execute(char** args){
+    //ejecutar funciones
+    if (!strcmp(args[0],"exec")){
+        execProgram(args[1],(int)args[2]-48);
+    }else if (!strcmp(args[0],"free")){
+        freeProgram(args[1]);
+    }else if (!strcmp(args[0],"heap")){
+        assignHeap(args[1],(int)args[2]-48);
+    }else if (!strcmp(args[0],"mem")){
+        checkMem(args[1]);
+    }else if (!strcmp(args[0],"read")){
+        //
+    }else if (!strcmp(args[0],"write")){
+        //
+    }
+}
+
 int main(){
 	int memArray[32];
+
+    char *line;
+    char **args;
+
+    size_t line_size,valor = 0;
+    size_t *n_args = &valor;
+
+    while(1) {
+        //printf("Ingrese funcion: "); //doesn't work
+        line = read_line();
+        line_size = strlen(line);
+        args = split_line(line, n_args);
+        execute(args);
+        free(line);
+        break; //sacar despues
+    }
+
+    return 0;
 }
